812f947712d7b00d51538ec258421fcb28ca35fc
FortuneMal <fmalaza512@gmail.com>
2024-10-22 15:12:28 +0000

    lol

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,6 +1,5 @@
 import logo from "./holberton-logo.jpg";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
@@ -12,9 +11,7 @@ function App() {
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
+      <div className="App-footer">Copyright 2020 - holberton School</div>
     </div>
   );
 }
